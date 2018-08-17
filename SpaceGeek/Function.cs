be501778b7bb024dd08a5c26e2b7d858533b4cc9
jgrivas <jorgegomezrivas@gmail.com>
2018-08-18 00:11:19 +0200

    Use tutorial code.

--- SpaceGeek/Function.cs
+++ SpaceGeek/Function.cs
@@ -1,26 +1,63 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Alexa.NET.Response;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
-using Alexa.NET.Response;
-using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace SpaceGeek
 {
     public class Function
     {
+        public List<FactResource> GetResources()
+        {
+            List<FactResource> resources = new List<FactResource>();
+            FactResource enUSResource = new FactResource("en-US");
+            enUSResource.SkillName = "American Science Facts";
+            enUSResource.GetFactMessage = "Here's your science fact: ";
+            enUSResource.HelpMessage = "You can say tell me a science fact, or, you can say exit... What can I help you with?";
+            enUSResource.HelpReprompt = "You can say tell me a science fact to start";
+            enUSResource.StopMessage = "Goodbye!";
+            enUSResource.Facts.Add("A year on Mercury is just 88 days long.");
+            enUSResource.Facts.Add("Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.");
+            enUSResource.Facts.Add("Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.");
+            enUSResource.Facts.Add("On Mars, the Sun appears about half the size as it does on Earth.");
+            enUSResource.Facts.Add("Earth is the only planet not named after a god.");
+            enUSResource.Facts.Add("Jupiter has the shortest day of all the planets.");
+            enUSResource.Facts.Add("The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.");
+            enUSResource.Facts.Add("The Sun contains 99.86% of the mass in the Solar System.");
+            enUSResource.Facts.Add("The Sun is an almost perfect sphere.");
+            enUSResource.Facts.Add("A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.");
+            enUSResource.Facts.Add("Saturn radiates two and a half times more energy into space than it receives from the sun.");
+            enUSResource.Facts.Add("The temperature inside the Sun can reach 15 million degrees Celsius.");
+            enUSResource.Facts.Add("The Moon is moving approximately 3.8 cm away from our planet every year.");
+
+            resources.Add(enUSResource);
+            return resources;
+        }
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            var response = new SkillResponse();
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
             response.Response.ShouldEndSession = false;
             IOutputSpeech innerResponse = null;
             var log = context.Logger;
+            log.LogLine($"Skill Request Object:");
+            log.LogLine(JsonConvert.SerializeObject(input));
 
             var allResources = GetResources();
             var resource = allResources.FirstOrDefault();
@@ -29,11 +66,13 @@ namespace SpaceGeek
             {
                 log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
                 innerResponse = new PlainTextOutputSpeech();
-                (innerResponse as PlainTextOutputSpeech).Text = EmitNewFact(resource, true);
+                (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, true);
+
             }
             else if (input.GetRequestType() == typeof(IntentRequest))
             {
                 var intentRequest = (IntentRequest)input.Request;
+
                 switch (intentRequest.Intent.Name)
                 {
                     case "AMAZON.CancelIntent":
@@ -56,12 +95,12 @@ namespace SpaceGeek
                     case "GetFactIntent":
                         log.LogLine($"GetFactIntent sent: send new fact");
                         innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = EmitNewFact(resource, false);
+                        (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
                         break;
                     case "GetNewFactIntent":
                         log.LogLine($"GetFactIntent sent: send new fact");
                         innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = EmitNewFact(resource, false);
+                        (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
                         break;
                     default:
                         log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
@@ -73,48 +112,35 @@ namespace SpaceGeek
 
             response.Response.OutputSpeech = innerResponse;
             response.Version = "1.0";
+            log.LogLine($"Skill Response Object...");
+            log.LogLine(JsonConvert.SerializeObject(response));
             return response;
         }
 
-        private List<FactResource> GetResources()
+        public string emitNewFact(FactResource resource, bool withPreface)
         {
-            var resources = new List<FactResource>();
-            var enUSResource = new FactResource("en-US");
-            enUSResource.SkillName = "American Space Facts";
-            enUSResource.GetFactMessage = "Here's your fact: ";
-            enUSResource.HelpMessage = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-            enUSResource.HelpReprompt = "What can I help you with?";
-            enUSResource.StopMessage = "Goodbye!";
-            enUSResource.Facts.Add("A year on Mercury is just 88 days long.");
-            enUSResource.Facts.Add("Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.");
-            enUSResource.Facts.Add("Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.");
-            enUSResource.Facts.Add("On Mars, the Sun appears about half the size as it does on Earth.");
-            enUSResource.Facts.Add("Earth is the only planet not named after a god.");
-            enUSResource.Facts.Add("Jupiter has the shortest day of all the planets.");
-            enUSResource.Facts.Add("The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.");
-            enUSResource.Facts.Add("The Sun contains 99.86% of the mass in the Solar System.");
-            enUSResource.Facts.Add("The Sun is an almost perfect sphere.");
-            enUSResource.Facts.Add("A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.");
-            enUSResource.Facts.Add("Saturn radiates two and a half times more energy into space than it receives from the sun.");
-            enUSResource.Facts.Add("The temperature inside the Sun can reach 15 million degrees Celsius.");
-            enUSResource.Facts.Add("The Moon is moving approximately 3.8 cm away from our planet every year.");
-
-            resources.Add(enUSResource);
-            return resources;
-        }
-
-        private string EmitNewFact(FactResource resource, bool withPreface)
-        {
-            var stringBuilder = new StringBuilder();
+            Random r = new Random();
             if (withPreface)
-            {
-                stringBuilder.Append(resource.GetFactMessage);
-            }
-
-            var random = new Random();
-            stringBuilder.Append(resource.Facts[random.Next(resource.Facts.Count)]);
-
-            return stringBuilder.ToString();
+                return resource.GetFactMessage + resource.Facts[r.Next(resource.Facts.Count)];
+            return resource.Facts[r.Next(resource.Facts.Count)];
         }
+
+    }
+
+    public class FactResource
+    {
+        public FactResource(string language)
+        {
+            this.Language = language;
+            this.Facts = new List<string>();
+        }
+
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public List<string> Facts { get; set; }
+        public string GetFactMessage { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
     }
 }
