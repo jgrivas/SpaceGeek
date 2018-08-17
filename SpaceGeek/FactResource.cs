98f01d25dea9c56d2984f35c393aceda66ba3b4a
jgrivas <jorgegomezrivas@gmail.com>
2018-08-17 23:26:20 +0200

    Include tutorial code.

---
+++ SpaceGeek/FactResource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SpaceGeek
+{
+    internal class FactResource
+    {
+        public FactResource(string language)
+        {
+            this.Language = language;
+        }
+
+        public string Language { get; set; }
+        public string SkillName { get; set; }
+        public List<string> Facts { get; } = new List<string>();
+        public string GetFactMessage { get; set; }
+        public string HelpMessage { get; set; }
+        public string HelpReprompt { get; set; }
+        public string StopMessage { get; set; }
+    }
+}
